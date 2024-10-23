d2c85bfd332bd42b86bd4c637379cbeac55da297
Maisha Nawar <mnawar@Zipcoders-MacBook-Pro.local>
2024-10-22 22:20:31 -0400

    experiment with ticketMachine

--- src/main/java/TicketMachine.java
+++ src/main/java/TicketMachine.java
@@ -31,6 +31,9 @@ public class TicketMachine {
         ticketNumber = 0;
     }
 
+
+
+
     /**
      * Return the price of a ticket.
      */
@@ -87,4 +90,29 @@ public class TicketMachine {
 
         return "Ticket price: " + price + " cents. " + "Your total is " + total + ".";
     }
+
+
+
+    public static void main(String[] args){
+        TicketMachine machine1 = new TicketMachine(40);
+        System.out.println(machine1.getPrice());
+        System.out.println(machine1.insertMoney(100));
+        System.out.println(machine1.getBalance());
+        System.out.println(machine1.printTicket());
+        //0 is returned if we check the machine’s balance after it has printed a ticket
+        System.out.println(machine1.getBalance());
+
+        TicketMachine machine2 = new TicketMachine(68);
+        System.out.println(machine2.getPrice());
+        System.out.println(machine2.insertMoney(100));
+        System.out.println(machine2.getBalance());
+        System.out.println(machine2.printTicket());
+        //0 is returned if we check the machine’s balance after it has printed a ticket
+        System.out.println(machine1.getBalance());
+
+
+
+
+    }
+
 }
